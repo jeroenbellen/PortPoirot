6e3922e55e0f6dc07484f11e36f14779aa230ec9
Jeroen Bellen <bellenjeroen@gmail.com>
2020-03-11 14:27:14 +0100

    Moves table model logic

---
+++ port_table_model.py
@@ -0,0 +1,31 @@
+from PySide2.QtCore import QAbstractTableModel, Qt
+
+class PortTableModel(QAbstractTableModel):
+
+    def __init__(self, port_lines):
+        super(PortTableModel, self).__init__()
+        self._data = self._map_data(port_lines)
+        self.header_labels = ['Ip', 'Port', 'Pid']
+
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return len(self._data[0])
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+            return self.header_labels[section]
+
+    def _map_data(self, port_lines):
+        return list(
+            map(self._map_2_array, port_lines)
+        )
+
+    def _map_2_array(self, line):
+        return [line.ip, line.port, line.pid]
--- qt_poc.py
+++ qt_poc.py
@@ -6,35 +6,11 @@ from port_data_provider import PortDataProvider
 
 from filter_widget import FilterWidget
 from port_filter_proxy_model import PortFilterProxyModel
-
-class TableModel(QtCore.QAbstractTableModel):
-
-    def __init__(self, data):
-        super(TableModel, self).__init__()
-        self._data = data
-        self.header_labels = ['Ip', 'Port', 'Pid']
-
-
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            return self._data[index.row()][index.column()]
-
-    def rowCount(self, index):
-        return len(self._data)
-
-    def columnCount(self, index):
-        return len(self._data[0])
-
-    def headerData(self, section, orientation, role):
-        if role == QtCore.Qt.DisplayRole and orientation == QtCore.Qt.Horizontal:
-            return self.header_labels[section]
+from port_table_model import PortTableModel
     
 
 
 data_provider = PortDataProvider()
-
-def map_2_array(line):
-    return [line.ip, line.port, line.pid]
 
 proxy = PortFilterProxyModel()
 
@@ -42,11 +18,7 @@ class PortTableView(QtWidgets.QTableView):
     def __init__(self):
         super().__init__()
 
-        data = list(
-            map(map_2_array, data_provider.getAllActivePorts())
-        )
-
-        model = TableModel(data)
+        model = PortTableModel(data_provider.getAllActivePorts())
         proxy.setSourceModel(model)
 
         
