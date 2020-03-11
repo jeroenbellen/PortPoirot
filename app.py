a29b3b49d1370fe7f3440584812de251d5b1df55
Jeroen Bellen <bellenjeroen@gmail.com>
2020-03-11 14:43:38 +0100

    Moves logic to app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import sys 
+from PySide2.QtWidgets import QApplication
+from port_data_provider import PortDataProvider
+from main_window import MainWindow
+
+data_provider = PortDataProvider()
+
+app = QApplication(sys.argv)
+
+window = MainWindow(data_provider.getAllActivePorts())
+
+window.resize(500, 600)
+window.setWindowTitle('Port Poirot')
+
+window.show()
+
+app.exec_()
--- qt_poc.py
+++ qt_poc.py
@@ -2,15 +2,5 @@ import sys
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import Qt
 
-from port_data_provider import PortDataProvider
-from main_window import MainWindow
 
 
-data_provider = PortDataProvider()
-
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow(data_provider.getAllActivePorts())
-window.resize(500, 600)
-window.setWindowTitle('Port Poirot')
-window.show()
-app.exec_()
