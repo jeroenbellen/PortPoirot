f87a412682098919099829ccf5951b946297b809
Jeroen Bellen <bellenjeroen@gmail.com>
2020-03-21 17:46:25 +0100

    Build using python3

--- build-macos.sh
+++ build-macos.sh
@@ -1,4 +1,4 @@
-python -m PyInstaller --icon detective.icns --noconfirm --clean -n "Port Poirot" --windowed __main__.py
+python3 -m PyInstaller --icon detective.icns --noconfirm --clean -n "Port Poirot" --windowed __main__.py
 
-python alter_info_plist.py
+python3 alter_info_plist.py
 
